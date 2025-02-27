267f841ba59963625c3049e7a448cd01d99b3a04
Fränk Klein <frank@kleinconsulting.lu>
2025-02-27 15:29:55 +0100

    Add initial plugin file.

---
+++ wpdc-teachable-course-curriculum.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Plugin Name: WPDC - Teachable Course Curriculum
+ * Description: Retrieves the curriculum of a course from Teachable, and displays it as a block.
+ * Requires Plugins: teachable
+ * Update URI: false
+ */
